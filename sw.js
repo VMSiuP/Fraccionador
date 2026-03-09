0de84571d6d6cb51985bc9f84e80800fb442f33a
Víctor Manuel Siu Puyén <vmsiup@gmail.com>
2026-03-09 16:57:12 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
-const CACHE = 'v-siu-v3';
-const assets = ['./', './index.html', './style.css', './app.js', './manifest.json', './logo.png'];
+const CACHE_NAME = 'fraccionador-v1';
+// Usamos rutas relativas para que funcione en cualquier subcarpeta de GitHub
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './logo.png'
+];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
