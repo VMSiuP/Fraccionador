cf08ed8b53310985465791b703296d144910833d
Víctor Manuel Siu Puyén <vmsiup@gmail.com>
2026-03-09 16:29:14 -0500

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'v-siu-calc-v2';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './logo.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
